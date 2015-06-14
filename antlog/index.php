c1ce5053f5fe62522c2d45815899778dd7bc8940
GaryA <gary.aylward@ntlworld.com>
2015-06-14 22:01:09 +0100

    rename web folder to antlog

--- antlog/index.php
+++ antlog/index.php
@@ -3,15 +3,15 @@
 // If the package is installed via composer with --no-dev then yii2-dev-env will not be included
 // If the development packages are included then including dev-env.php will set the YII_DEBUG and
 // YII_ENV constants to their development/debug values, otherwise they default to production values
-$dev_env = __DIR__ . '/../vendor/garya/yii2-dev-env/dev-env.php';
+$dev_env = __DIR__ . '/../../antlog/vendor/garya/yii2-dev-env/dev-env.php';
 if(file_exists($dev_env))
 {
 	include $dev_env;
 }
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../antlog/vendor/autoload.php');
+require(__DIR__ . '/../../antlog/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+$config = require(__DIR__ . '/../../antlog/config/web.php');
 
 (new yii\web\Application($config))->run();
