6b5826a7373a74ca4f9e8a26352c69d6ee5f56e1
GaryA <gary.aylward@ntlworld.com>
2015-08-12 23:00:47 +0100

    Adding progress bar to upload - part 1

--- views/site/upload.php
+++ views/site/upload.php
@@ -13,6 +13,10 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $form->field($model, 'uploadFile')->fileInput() ?>
 
     <button>Submit</button>
-
+	<div id="progress-bar" class="progress" style="display:none">
+		<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+		<span class="sr-only">Please wait...</span>
+	</div>
+</div>
 <?php ActiveForm::end() ?>
 </div>
