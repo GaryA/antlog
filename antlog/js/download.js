6823c464d6dc2a6d3b7766c36b1f6e3c850fc185
GaryA <gary.aylward@ntlworld.com>
2016-04-25 22:07:05 +0100

    Fix file path bug in download.js

--- antlog/js/download.js
+++ antlog/js/download.js
@@ -7,5 +7,5 @@ function Redirect()
 {
 	// Redirect to a new URL, since the redirects to an SQL file it just causes a file
 	// download, not a visible page redirect. The user appears to stay on the original page
-	window.location = "/db/export";
+	window.location = "../db/export";
 }
