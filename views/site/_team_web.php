b59fadebe93d1c4081382b4918dfa8c4e1fd3ddf
GaryA <gary.aylward@ntlworld.com>
2016-07-18 22:40:34 +0100

    Add help page for team and robot creation

--- views/site/_team_web.php
+++ views/site/_team_web.php
@@ -1,4 +1,35 @@
+<h3>Register Your Team</h3>
+<p>
+First, you have to register your team. Click the &quot;New Team&quot; link and fill in the details. 
+Your email address is only used to enable the password reset function to work; you won't get spammed!
+</p>
+<h3>Create Your Robots</h3>
+<p>
+Go to the Robots page and click the &quot;New Robot&quot; button. Fill in the form ensuring you select 
+the correct weight class, type etc. You can update the details of a robot by going to its page and 
+clicking the &quot;Update&quot; button. Once a robot has competed in an event you cannot change many of the 
+details but you can make a robot inactive and replace it with a new one.
+</p>
+<p>
+Inactive robots remain in the database and in the results of past events but cannot be entered in 
+new events.
+</p>
+<h3>Sign Up To Events</h3>
+<p>
+Note that Antlog treats each competition at a single meeting as an &quot;Event&quot;. You should only 
+sign up antweights to an AWS; fleas, nanos etc will be handled separately. Often they will just be 
+handled on paper at the meeting.
+</p>
+<p>
+Click the &quot;Sign Up&quot; button on the event page and select the robot that you wish to enter. 
+Repeat for multiple robots as necessary. You can also delete any entries that will not make it to the 
+event.
+</p>
+<p>
+Keeping your entries correct really helps the event organiser on the day. Also ensure that you you sign 
+up before the deadline given by the event organiser. Robots may be signed up on the day but it&apos;s 
+much easier for everyone if everything is sorted out in advance.</p>
+<h3>At An Event</h3>
 <p>
 Please refer to the <a href="https://github.com/GaryA/antlog/wiki/Event-Team-Use">AntLog wiki</a>
 </p>
-<h3>Help for Teams Online</h3>
