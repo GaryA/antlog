18d7ec8c9320784b857d755e4b4c05072119e07b
GaryA <gary.aylward@ntlworld.com>
2018-06-05 20:40:47 +0100

    Updated maintenance message to show v3.1

--- views/site/maintenance.php
+++ views/site/maintenance.php
@@ -1,10 +1,10 @@
 <?php
-$this->title = 'AntLog 3.0';
+$this->title = 'AntLog 3.1';
 ?>
 <div class="site-index">
 
 	<div class="jumbotron">
-		<h1>AntLog 3.0</h1>
+		<h1>AntLog 3.1</h1>
 
 		<p class="lead">AntLog is currently offline for maintenance, please check back later.</p>
 
